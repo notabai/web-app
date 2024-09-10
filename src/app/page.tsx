a1b285b2ec3b2e5ea3fa58633864fd45bd5d83cf
wd021 <dannywitters@gmail.com>
2024-09-10 23:12:26 +0100

    update landing page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
     <div className="h-screen flex flex-col items-center justify-center text-center px-6">
       <Image src="/logo.png" alt="No Tab Logo" width={300} height={300} />
       <div className="text-2xl mt-12">
-        Use the internet without a browser. Let AI agents do the work for you.
+        Let&apos;s get rid of all the tabs. Let AI agents browse the web for
+        you.
       </div>
       <div className="text-2xl mt-4">Coming soon...</div>
     </div>
