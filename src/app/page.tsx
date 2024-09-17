6ebeb53d4aedc8fc2127bfb0bde43f0514f2dc6a
wd021 <dannywitters@gmail.com>
2024-09-17 23:59:45 +0100

    change logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,7 @@ export default function Home() {
     <div className="h-screen flex flex-col items-center justify-center text-center px-6">
       <Image src="/logo.png" alt="No Tab Logo" width={300} height={300} />
       <div className="text-2xl mt-12">
-        Let&apos;s get rid of all the tabs. Let AI agents browse the web for
-        you.
+        Use AI agents to browser the internet.
       </div>
       <div className="text-2xl mt-4">Coming soon...</div>
     </div>
